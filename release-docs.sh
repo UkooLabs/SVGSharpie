ff7ea8a66540dae27a57264765f7e7033b99f38c
Peter Tribe <ptribe@pnimedia.com>
2019-02-14 07:15:37 -0800

    Update: doc build

--- release-docs.sh
+++ release-docs.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 set -e
 
+export VSINSTALLDIR="C:\Program Files (x86)\Microsoft Visual Studio\2017\Community"
+export VisualStudioVersion="15.0"
+
 docfx ./docs/docfx.json
 
 SOURCE_DIR=$PWD
