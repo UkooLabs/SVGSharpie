38508fff11ece97b3cc537af657ed90984552594
Peter Tribe <ptribe@pnimedia.com>
2019-02-13 23:41:17 -0800

    Added: docfx

--- release-docs.sh
+++ release-docs.sh
@@ -1,20 +1,17 @@
 #!/bin/sh
 set -e
 
-export VSINSTALLDIR="C:\Program Files (x86)\Microsoft Visual Studio\2017\Community"
-export VisualStudioVersion="15.0"
-
 docfx ./docs/docfx.json
 
 SOURCE_DIR=$PWD
-TEMP_REPO_DIR=$PWD/../svgsharpie-docfx-gh-pages
+TEMP_REPO_DIR=$PWD/../svgsharpie-docfx
 
 echo "Removing temporary doc directory $TEMP_REPO_DIR"
 rm -rf $TEMP_REPO_DIR
 mkdir $TEMP_REPO_DIR
 
 echo "Cloning the repo with the gh-pages branch"
-git clone https://github.com/equinox2k/svgsharpie.git --branch gh-pages $TEMP_REPO_DIR
+git clone https://github.com/equinox2k/equinox2k.github.io.git --branch master $TEMP_REPO_DIR
 
 echo "Clear repo directory"
 cd $TEMP_REPO_DIR
@@ -26,4 +23,4 @@ cp -r $SOURCE_DIR/docs/_site/* .
 echo "Push the new docs to the remote branch"
 git add . -A
 git commit -m "Update generated documentation"
-git push origin gh-pages
+git push origin master
