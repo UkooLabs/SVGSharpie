2307ea04d88e8111c78176d16f42a7ef04d0567b
Peter Tribe <ptribe@pnimedia.com>
2019-02-14 10:21:19 -0800

    Debug: token

--- tests/CodeCoverage/CodeCoverage.cmd
+++ tests/CodeCoverage/CodeCoverage.cmd
@@ -1,6 +1,5 @@
 @echo off
 
-
 cd tests\CodeCoverage
 
 nuget restore packages.config -PackagesDirectory .
@@ -15,6 +14,9 @@ rem The -threshold options prevents this taking ages...
 tests\CodeCoverage\OpenCover.4.6.519\tools\OpenCover.Console.exe -target:"dotnet.exe" -targetargs:"test tests\EquinoxLabs.SVGSharpie.ImageSharp.Tests\EquinoxLabs.SVGSharpie.ImageSharp.Tests.csproj -c Release -f netcoreapp2.1 /p:codecov=true" -register:user -threshold:10 -oldStyle -safemode:off -output:.\SVGSharpie.Coverage.xml -hideskipped:All -returntargetcode -filter:"+[EquinoxLabs.SVGSharpie*]*" 
 
 if %errorlevel% neq 0 exit /b %errorlevel%
+
+echo hello
+echo "%codecov_token%"
 
 SET PATH=C:\python37;C:\python37\Scripts;%PATH%
 python -m pip install --upgrade pip
