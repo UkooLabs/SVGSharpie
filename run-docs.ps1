a8cafa8882c30b3f50f9170bdf4f68ab50759c70
equinox2k <%github_email%>
2019-02-14 08:50:58 -0800

    Update: config

--- run-docs.ps1
+++ run-docs.ps1
@@ -6,8 +6,12 @@ param (
 )
 
 Write-Host "Processing Documents:"
+Write-Host $user_profile
+Write-Host $github_access_token
+Write-Host $github_email
+Write-Host $github_user
 
 git config --global credential.helper store
 Add-Content "$user_profile\.git-credentials" "https://$($github_access_token):x-oauth-basic@github.com`n"
-git config --global user.email $github_email
-git config --global user.name $github_user
+git config --global user.email %github_email%
+git config --global user.name %github_user%
