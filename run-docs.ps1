de9a7359eac2eb26c9024674adf9d010f0285658
equinox2k <%github_email%>
2019-02-14 08:18:56 -0800

    Update: powershell

--- run-docs.ps1
+++ run-docs.ps1
@@ -1,1 +1,11 @@
-Write-Host "Hello World:"
+param (
+    [Parameter(Mandatory=$true)][string]$user_profile,
+    [Parameter(Mandatory=$true)][string]$github_access_token,
+    [Parameter(Mandatory=$true)][string]$github_email,
+    [Parameter(Mandatory=$true)][string]$github_user
+)
+
+git config --global credential.helper store
+Add-Content "$user_profile\.git-credentials" "https://$($github_access_token):x-oauth-basic@github.com`n"
+git config --global user.email $github_email
+git config --global user.name $github_user
