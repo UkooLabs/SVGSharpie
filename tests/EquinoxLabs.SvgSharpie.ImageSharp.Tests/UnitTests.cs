63beab82d833fb0683b1787af19008c5a8713ed3
Peter Tribe <ptribe@pnimedia.com>
2019-02-16 13:44:08 -0800

    Update: tidy test

---
+++ tests/EquinoxLabs.SvgSharpie.ImageSharp.Tests/UnitTests.cs
@@ -0,0 +1,20 @@
+using System;
+using System.IO;
+using Xunit;
+
+namespace EQL.SVGSharpie.ImageSharp.Tests
+{
+    public class UnitTests
+    {
+        [Fact]
+        public void Test1()
+        {
+            string path = Path.GetTempFileName();
+            string testData = Guid.NewGuid().ToString();
+            File.WriteAllText(path, testData);
+            string data = File.ReadAllText(path);
+            Assert.Equal(testData, data);
+            File.Delete(path);
+        }
+    }
+}
